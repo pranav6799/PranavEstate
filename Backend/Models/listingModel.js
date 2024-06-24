48daa386e7c7ea77800d002d6c01ecf20fae1f4e
pranav6799 <150272347+pranav6799@users.noreply.github.com>
2024-06-24 18:50:30 +0530

    Create listingModel.js

---
+++ Backend/Models/listingModel.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose')
+
+const listingSchema = mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    }, 
+    address:{
+        type:String,
+        required:true
+    },
+    regularPrice:{
+        type:Number,
+        required:true
+    },
+    discountPrice:{
+        type:Number,
+        required:true
+    },
+    bathrooms:{
+        type:Number,
+        required:true
+    },
+    bedrooms:{
+        type:Number,
+        required:true
+    },
+    furnished:{
+        type:Boolean,
+        required:true
+    },
+    parking:{
+        type:Boolean,
+        required:true
+    },
+    type:{
+        type:String,
+        required:true
+    },
+    offer:{
+        type:Boolean,
+        required:true
+    },
+    imageUrls:{
+        type:Array,
+        required:true
+    },
+    user:{
+        type:mongoose.Schema.ObjectId,
+        ref:'User',
+        required:true
+    }
+
+},{timestamps:true})
+
+const Listing = mongoose.model('Listing',listingSchema)
+
+module.exports = Listing
