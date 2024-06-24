f1480d84b9f071f2076935ef2f4d6d50dce7eb61
pranav6799 <150272347+pranav6799@users.noreply.github.com>
2024-06-24 23:17:13 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,12 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    proxy:{
-    '/api':{
-      target:'http://localhost:8080',
-      secure:false
-    }
-  }
+   
 },
   plugins: [react()],
   
