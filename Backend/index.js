faa344584761929faf1e3109f8a9ad0127caa051
pranav6799 <150272347+pranav6799@users.noreply.github.com>
2024-06-24 22:15:59 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -10,10 +10,20 @@ const path = require('path')
 
 connectDb()
 
+const allowedOrigins = ['http://localhost:5173', 'https://pranavestate.onrender.com'];
 
+// Middleware setup
 app.use(cors({
-    origin: 'http://localhost:8080',
-  credentials: true
+    origin: function (origin, callback) {
+        // Allow requests with no origin (like mobile apps or curl requests)
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    },
+    credentials: true
 }))
 app.use(express.json())
 app.use(cookieParser())
