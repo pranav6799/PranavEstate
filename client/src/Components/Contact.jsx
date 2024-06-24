8be42d23f85640caa9f31d3d34bf495bd69bdfc9
pranav6799 <150272347+pranav6799@users.noreply.github.com>
2024-06-24 19:13:27 +0530

    Create Contact.jsx

---
+++ client/src/Components/Contact.jsx
@@ -0,0 +1,39 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+
+const Contact = ({listing}) => {
+const [landLord, setLandLord]=useState(null)
+const [message,setMessage]=useState('')
+console.log(message)
+
+const onChange = (e)=>{
+setMessage(e.target.value)
+}
+
+useEffect(()=>{
+  const fetchUser = async()=>{
+  const {data} = await axios.get(`http://localhost:8080/api/user/getUser/${listing.user}`,{
+    withCredentials:true
+  })
+  setLandLord(data.user)
+  }
+  fetchUser()
+
+},[listing])
+  return (
+    <>
+    {landLord && (
+      <div className="flex flex-col gap-2">
+        <p>Contact <span className='font-semibold'> {landLord.username}</span> for <span className='font-semibold'> {listing.name.toLowerCase()} </span></p>
+        <textarea onChange={onChange} name='message' id='message' cols='30' rows='2' value={message} placeholder='Enter your message here'className='w-full border p-3 rounded-lg '></textarea>
+        <Link to={`mailto:${landLord.email}? subject=Regarding ${listing.name} &body${message}`} className='bg-slate-700 text-white text-center p-3 upercase rounded-lg hover:opacity-95'>Send Message</Link>
+      </div>
+
+    )
+    }
+    </>
+  )
+}
+
+export default Contact
