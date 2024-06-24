ca11cd580f2a23559079b76a155dda601b5c4c40
pranav6799 <150272347+pranav6799@users.noreply.github.com>
2024-06-24 18:47:42 +0530

    Create index.js

---
+++ Backend/index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const dotenv = require('dotenv').config()
+const app = express()
+const connectDb = require('./connectDb')
+const userRouter = require('./Routes/userRouter')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const listingRouter = require('./Routes/listingRoute')
+const path = require('path')
+
+connectDb()
+const __dirname = path.resolve()
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+  credentials: true
+}))
+app.use(express.json())
+app.use(cookieParser())
+app.use('/api/user',userRouter)
+app.use('/api/listing',listingRouter)
+
+app.use(express.static(path.join(__dirname,'/client/dist')))
+app.get('*',(req,resp)=>{
+    resp.sendFile(path.join(__dirname,'client','dist','index.html'))
+})
+
+app.listen(8080,()=>{
+    console.log('server running on port 8080')
+})
+
+
+
+app.use((err,req,resp,next) =>{
+    const statusCode = err.statusCode || 500
+    const message = err.message || 'Internal Server Error'
+    resp.status(statusCode).json({
+        status:false,
+        statusCode,
+        message
+    })
+})
