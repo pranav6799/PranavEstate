4e2d38e5847cf351bff393df488b91424e679d4f
pranav6799 <150272347+pranav6799@users.noreply.github.com>
2024-06-24 19:18:21 +0530

    Add files via upload

---
+++ client/src/firebase.js
@@ -0,0 +1,44 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDacQ2VVPE8ygGdXRCkO_A8v15cLn7SX_w",
+  authDomain: "mern-estate-e089d.firebaseapp.com",
+  projectId: "mern-estate-e089d",
+  storageBucket: "mern-estate-e089d.appspot.com",
+  messagingSenderId: "627619032135",
+  appId: "1:627619032135:web:e7d2105b208552ad0b1562"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// service firebase.storage {
+//   match /b/{bucket}/o {
+//     match /{allPaths=**} {
+//       allow read;
+//       allow write: if 
+//       request.resource.size <2 *1024*1024 && 
+//       request.resource.contentType.matches('image/.*')
+//     }
+//   }
+// }
